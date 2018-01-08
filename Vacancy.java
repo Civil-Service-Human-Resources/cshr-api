788998c48ef898a4be48ca39bbbf25c8b21de6d8
Seamus Singh <35099822+seamussingh@users.noreply.github.com>
2018-01-08 15:01:20 +0000

    Use new search query
    This needs the associated test to be ignored until Postgres docker container is available to allow the test to be run again as the new query is not supported by h2 in memory database.

---
+++ Vacancy.java
@@ -0,0 +1,66 @@
+package uk.gov.cshr.vcm.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "vacancies")
+@SequenceGenerator(name = "vacancies_sequence", sequenceName = "vacancies_sequence", allocationSize = 1)
+public class Vacancy implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "vacancies_sequence")
+    private long id;
+
+    private @NonNull
+    String title;
+
+    private @NonNull
+    String description;
+
+    private @NonNull
+    String location;
+
+    private @NonNull
+    String grade;
+
+    private @NonNull
+    String role;
+
+    private @NonNull
+    String responsibilities;
+
+    private @NonNull
+    String workingHours;
+
+    private @NonNull
+    String closingDate;
+
+    private @NonNull
+    String contactName;
+
+    private @NonNull
+    String contactDepartment;
+
+    private @NonNull
+    String contactEmail;
+
+    private @NonNull
+    String contactTelephone;
+
+    private @NonNull
+    String eligibility;
+
+    private int salaryMin;
+
+    private int salaryMax;
+
+    private int numberVacancies;
+
+}
