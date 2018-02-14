dc182d116651ac4e1ef6f80cbdfa5f9b943588ff
Gordon <gordon.mackay@valtech.co.uk>
2018-02-14 08:50:00 +0000

    Added sql tests.

--- vcm/src/test/java/uk/gov/cshr/vcm/repository/SearchQueryBuilderTest.java
+++ vcm/src/test/java/uk/gov/cshr/vcm/repository/SearchQueryBuilderTest.java
@@ -3,7 +3,8 @@ package uk.gov.cshr.vcm.repository;
 import com.google.common.io.ByteStreams;
 import java.io.IOException;
 import java.io.InputStream;
-import static org.hamcrest.CoreMatchers.equalTo;
+import org.hamcrest.CoreMatchers;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.After;
 import org.junit.Before;
@@ -17,6 +18,7 @@ import uk.gov.cshr.vcm.model.VacancySearchParameters;
  * Tests {@link SearchQueryBuilder}
  */
 public class SearchQueryBuilderTest {
+
     private static final String ALL_PARAMETERS_SELECT_VALUES_QUERY = "/repository/AllParametersSuppliedSelectValuesQuery.sql";
     private static final String BLANK = "";
     private static final String BRISTOL = "Bristol";
@@ -55,14 +57,44 @@ public class SearchQueryBuilderTest {
         return SearchParameters.builder().vacancySearchParameters(vacancySearchParameters).coordinates(coordinates).build();
     }
 
-    private void doCountQueryTest(SearchParameters parameters, String expectedResourceFileName) throws IOException {
+    private SearchParameters buildParameters(String place, String keyword, String[] department, Integer salaryMin, Integer salaryMax) {
+
+        Location location = Location.builder()
+                .place(place)
+                .radius(30)
+                .build();
+
+        VacancySearchParameters vacancySearchParameters = VacancySearchParameters.builder()
+                .location(location)
+                .keyword(keyword)
+                .department(department)
+                .minSalary(salaryMin)
+                .maxSalary(salaryMax)
+                .build();
+
+        Coordinates coordinates = Coordinates.builder()
+                .latitude(51.4549291)
+                .longitude(-2.6278111)
+                .build();
+
+        return SearchParameters.builder()
+                .vacancySearchParameters(vacancySearchParameters)
+                .coordinates(coordinates)
+                .build();
+    }
+
+    private String doCountQueryTest(SearchParameters parameters, String expectedResourceFileName) throws IOException {
+
+        String actualQuery = "";
+
         try (InputStream inputStream = SearchQueryBuilderTest.class.getResourceAsStream(expectedResourceFileName)) {
+
             byte[] expectedQuery = ByteStreams.toByteArray(inputStream);
-
-            String actualQuery = builder.buildCountQuery(parameters);
-
-            assertThat(actualQuery, equalTo(new String(expectedQuery)));
+            actualQuery = builder.buildCountQuery(parameters);
+            assertThat(actualQuery, CoreMatchers.containsString(new String(expectedQuery)));;
         }
+
+        return actualQuery;
     }
 
     @Test
@@ -75,31 +107,20 @@ public class SearchQueryBuilderTest {
         doSelectValuesQueryTest(buildParameters(BRISTOL, null, null), LOCATION_SELECT_VALUES_QUERY);
     }
 
-    @Test
-    public void buildSelectValuesQuery_SalaryRange() throws IOException {
+    private String doSelectValuesQueryTest(SearchParameters parameters, String expectedResourceFileName)
+            throws IOException {
 
-        VacancySearchParameters vacancySearchParameters = VacancySearchParameters
-                .builder()
-                .location(Location.builder().place(BRISTOL).radius(30).build())
-                .minSalary(10000)
-                .build();
+        String actualQuery = "";
 
-        SearchParameters searchParameters = SearchParameters
-                .builder()
-                .vacancySearchParameters(vacancySearchParameters)
-                .build();
-
-        doSelectValuesQueryTest(searchParameters, SALARY_RANGE_QUERY);
-    }
-
-    private void doSelectValuesQueryTest(SearchParameters parameters, String expectedResourceFileName) throws IOException {
         try (InputStream inputStream = SearchQueryBuilderTest.class.getResourceAsStream(expectedResourceFileName)) {
+
             byte[] expectedQuery = ByteStreams.toByteArray(inputStream);
 
-            String actualQuery = builder.buildSelectValuesQuery(parameters);
-
-            assertThat(actualQuery, equalTo(new String(expectedQuery)));
+            actualQuery = builder.buildSelectValuesQuery(parameters);
+            assertThat(actualQuery, CoreMatchers.startsWith(new String(expectedQuery)));
         }
+
+        return actualQuery;
     }
 
     @Test
@@ -153,12 +174,115 @@ public class SearchQueryBuilderTest {
     }
 
     @Test
+    public void buildCountQuery_allParametersSuppliedWithSalaryRange() throws IOException {
+
+        int salaryMin = 100000;
+        int salaryMax = 200000;
+
+        String generatedQuery = doCountQueryTest(buildParameters(BRISTOL, KEYWORD, DEPARTMENTS, salaryMin, salaryMax),
+                "/repository/AllParametersSuppliedCountQuery.sql");
+
+        String expectedClause = "AND salary_max >= :salary_min AND salary_min <= :salary_max or salary_max is null";
+        assertThat(generatedQuery, CoreMatchers.containsString(expectedClause));
+    }
+
+    @Test
+    public void buildCountQuery_allParametersSuppliedWithSalaryMin() throws IOException {
+
+        int salaryMin = 100000;
+
+        String generatedQuery = doCountQueryTest(buildParameters(BRISTOL, KEYWORD, DEPARTMENTS, salaryMin, null),
+                "/repository/AllParametersSuppliedCountQuery.sql");
+
+        String expectedClause = "AND salary_max >= :salary_min";
+        String ommitedClause = " AND salary_min <= :salary_max or salary_max is null";
+
+        assertThat(generatedQuery, CoreMatchers.containsString(expectedClause));
+        assertThat(generatedQuery, not(CoreMatchers.containsString(ommitedClause)));
+    }
+
+    @Test
+    public void buildCountQuery_allParametersSuppliedWithSalaryMax() throws IOException {
+
+        int salaryMax = 100000;
+
+        String generatedQuery = doCountQueryTest(buildParameters(BRISTOL, KEYWORD, DEPARTMENTS, null, salaryMax),
+                "/repository/AllParametersSuppliedCountQuery.sql");
+
+        String expectedClause = "salary_min <= :salary_max or salary_max is null";
+        String ommitedClause = "salary_max >= :salary_min";
+
+        assertThat(generatedQuery, CoreMatchers.containsString(expectedClause));
+        assertThat(generatedQuery, not(CoreMatchers.containsString(ommitedClause)));
+    }
+
+    @Test
+    public void buildSelectQuery_allParametersSuppliedWithSalaryRange() throws IOException {
+
+        int salaryMin = 100000;
+        int salaryMax = 200000;
+
+        String generatedQuery = doSelectValuesQueryTest(buildParameters(BRISTOL, KEYWORD, DEPARTMENTS, salaryMin, salaryMax),
+                "/repository/AllParametersSuppliedSelectValuesQuery.sql");
+
+        String expectedClause = "AND salary_max >= :salary_min AND salary_min <= :salary_max or salary_max is null";
+        assertThat(generatedQuery, CoreMatchers.containsString(expectedClause));
+    }
+
+    @Test
+    public void buildSelectQuery_allParametersSuppliedWithSalaryMin() throws IOException {
+
+        int salaryMin = 100000;
+
+        String generatedQuery = doSelectValuesQueryTest(buildParameters(BRISTOL, KEYWORD, DEPARTMENTS, salaryMin, null),
+                "/repository/AllParametersSuppliedSelectValuesQuery.sql");
+
+        String expectedClause = "AND salary_max >= :salary_min";
+        String ommitedClause = " AND salary_min <= :salary_max or salary_max is null";
+
+        assertThat(generatedQuery, CoreMatchers.containsString(expectedClause));
+        assertThat(generatedQuery, not(CoreMatchers.containsString(ommitedClause)));
+    }
+
+    @Test
+    public void buildSelectQuery_allParametersSuppliedWithSalaryMax() throws IOException {
+
+        int salaryMax = 100000;
+
+        String generatedQuery = doSelectValuesQueryTest(buildParameters(BRISTOL, KEYWORD, DEPARTMENTS, null, salaryMax),
+                "/repository/AllParametersSuppliedSelectValuesQuery.sql");
+
+        String expectedClause = "salary_min <= :salary_max or salary_max is null";
+        String ommitedClause = "salary_max >= :salary_min";
+
+        assertThat(generatedQuery, CoreMatchers.containsString(expectedClause));
+        assertThat(generatedQuery, not(CoreMatchers.containsString(ommitedClause)));
+    }
+
+    @Test
     public void buildSelectValuesQuery_allParametersSupplied() throws IOException {
         doSelectValuesQueryTest(buildParameters(BRISTOL, KEYWORD, DEPARTMENTS), ALL_PARAMETERS_SELECT_VALUES_QUERY);
     }
 
     @Test
     public void buildSelectValuesQuery_allParametersSuppliedWithRogueDepartment() throws IOException {
-        doSelectValuesQueryTest(buildParameters(BRISTOL, KEYWORD, new String[]{"1","   ","2"}), ALL_PARAMETERS_SELECT_VALUES_QUERY);
+        doSelectValuesQueryTest(buildParameters(BRISTOL, KEYWORD, new String[]{"1", "   ", "2"}), ALL_PARAMETERS_SELECT_VALUES_QUERY);
+    }
+
+    @Test
+    public void buildSelectValuesQuery_SalaryRange() throws IOException {
+
+        VacancySearchParameters vacancySearchParameters = VacancySearchParameters
+                .builder()
+                .location(Location.builder().place(BRISTOL).radius(30).build())
+                .minSalary(10000)
+                .build();
+
+        SearchParameters searchParameters = SearchParameters
+                .builder()
+                .vacancySearchParameters(vacancySearchParameters)
+                .build();
+
+        doSelectValuesQueryTest(searchParameters, SALARY_RANGE_QUERY);
     }
 }
