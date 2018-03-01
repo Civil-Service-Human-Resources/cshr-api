d6af287b0b458190147fbcded78d8ecef8c6b3e4
Gordon <gordon.mackay@valtech.co.uk>
2018-03-01 08:34:47 +0000

    Consumes initial exception.

--- vcm/src/main/java/uk/gov/cshr/vcm/controller/exception/LocationServiceException.java
+++ vcm/src/main/java/uk/gov/cshr/vcm/controller/exception/LocationServiceException.java
@@ -1,5 +1,6 @@
 package uk.gov.cshr.vcm.controller.exception;
 
+import java.util.Arrays;
 import org.springframework.http.HttpStatus;
 
 public class LocationServiceException extends VacancyException {
@@ -11,6 +12,14 @@ public class LocationServiceException extends VacancyException {
         VacancyError vacancyError = VacancyError.builder()
                 .status(HttpStatus.SERVICE_UNAVAILABLE)
                 .message(SERVICE_UNAVAILABLE_MESSAGE).build();
+        super.setVacancyError(vacancyError);
+    }
+
+    public LocationServiceException(Exception e) {
+
+        VacancyError vacancyError = VacancyError.builder()
+                .status(HttpStatus.SERVICE_UNAVAILABLE)
+                .message(SERVICE_UNAVAILABLE_MESSAGE).errors(Arrays.asList(e.getMessage())).build();
         super.setVacancyError(vacancyError);
     }
 }
