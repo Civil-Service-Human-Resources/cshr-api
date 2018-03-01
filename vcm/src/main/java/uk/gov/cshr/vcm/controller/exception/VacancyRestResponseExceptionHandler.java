2dba9e2736aecfa351505673f995788c9ec96fbe
Gordon <gordon.mackay@valtech.co.uk>
2018-03-01 08:35:54 +0000

    Catches runtime exception and converted them to Vacancy errors.

--- vcm/src/main/java/uk/gov/cshr/vcm/controller/exception/VacancyRestResponseExceptionHandler.java
+++ vcm/src/main/java/uk/gov/cshr/vcm/controller/exception/VacancyRestResponseExceptionHandler.java
@@ -1,24 +1,24 @@
 package uk.gov.cshr.vcm.controller.exception;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import uk.gov.cshr.vcm.controller.exception.VacancyException;
 
 @ControllerAdvice
 public class VacancyRestResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
-//    @ExceptionHandler({VacancyClosedException.class})
-//    public ResponseEntity<Object> handleVacancyClosed(VacancyClosedException ex, WebRequest request) {
-//        return handleExceptionInternal(ex, ex.getVacancyError(), new HttpHeaders(), ex.getVacancyError().getStatus(), request);
-//    }
-
     @ExceptionHandler({VacancyException.class})
     public ResponseEntity<Object> handleLocationServiceException(VacancyException ex, WebRequest request) {
-
         return handleExceptionInternal(ex, ex.getVacancyError(), new HttpHeaders(), ex.getVacancyError().getStatus(), request);
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<Object> handleRuntimeException(RuntimeException ex, WebRequest request) {
+        VacancyError vacancyError = new VacancyError(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), null);
+        return handleExceptionInternal(ex, vacancyError, new HttpHeaders(), vacancyError.getStatus(), request);
     }
 }
