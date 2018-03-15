9eb3e3f4085a88fb5f82b019f4757f0ab9bad478
Gordon <gordon.mackay@valtech.co.uk>
2018-03-15 14:13:05 +0000

    fixed paths

--- vcm/src/test/resources/localscripts/run.sh
+++ vcm/src/test/resources/localscripts/run.sh
@@ -1,5 +1,7 @@
 #cd ../../../../../../rpg-candidate-interface-ui
 #npm start &
 cd ../../../../../../
+mvn -f location-service/location-service/pom.xml
+mvn -f location-service/location-service/pom.xml
 java -jar location-service/location-service/target/location-service-1.0-SNAPSHOT.jar &
 java -jar cshr-api/vcm/target/vcm-0.0.1.jar
