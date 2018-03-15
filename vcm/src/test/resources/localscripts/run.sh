56f33c00efcc5fa2dfbe70df990b66fb868be7a7
Gordon <gordon.mackay@valtech.co.uk>
2018-03-15 11:21:09 +0000

    Create database Run services locally

---
+++ vcm/src/test/resources/localscripts/run.sh
@@ -0,0 +1,5 @@
+cd ../../../../../../rpg-candidate-interface-ui
+npm start &
+cd ..
+java -jar location-service/location-service/target/location-service-1.0-SNAPSHOT.jar &
+java -jar cshr-api/vcm/target/vcm-0.0.1.jar
