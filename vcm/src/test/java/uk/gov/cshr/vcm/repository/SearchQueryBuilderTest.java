c3b7a13811d4bcce0d98a5ea0e149738b3029848
seamus <seamus.singh@valtech.co.uk>
2018-03-19 17:06:31 +0000

    CSHRCP-789 - Fixed bug that prevented overseas jobs from being returned

--- vcm/src/test/java/uk/gov/cshr/vcm/repository/SearchQueryBuilderTest.java
+++ vcm/src/test/java/uk/gov/cshr/vcm/repository/SearchQueryBuilderTest.java
@@ -18,17 +18,16 @@ import uk.gov.cshr.vcm.model.VacancySearchParameters;
  */
 public class SearchQueryBuilderTest {
 
-    private static final String ALL_PARAMETERS_SELECT_VALUES_QUERY = "/repository/AllParametersSuppliedSelectValuesQuery.sql";
+    private static final String ALL_LOCATION_PARAMETERS_QUERY_BODY = "/repository/AllLocationParametersQueryBody.sql";
+    private static final String ALL_PARAMETERS_QUERY_BODY = "/repository/AllParametersSuppliedQueryBody.sql";
     private static final String BLANK = "";
     private static final String BRISTOL = "Bristol";
     private static final String[] DEPARTMENTS = {"1", "2"};
     private static final String KEYWORD = "keyword";
-    private static final String LOCATION_DEPARTMENT_COUNT_QUERY = "/repository/LocationAndDepartmentCountQuery.sql";
-    private static final String LOCATION_DEPARTMENT_SELECT_VALUES_QUERY = "/repository/LocationAndDepartmentSelectValuesQuery.sql";
-    private static final String LOCATION_KEYWORD_COUNT_QUERY = "/repository/LocationAndKeywordCountQuery.sql";
-    private static final String LOCATION_KEYWORD_SELECT_VALUES_QUERY = "/repository/LocationAndKeywordSelectValuesQuery.sql";
-    private static final String LOCATION_COUNT_QUERY = "/repository/LocationOnlyCountQuery.sql";
-    private static final String LOCATION_SELECT_VALUES_QUERY = "/repository/LocationOnlySelectValuesQuery.sql";
+    private static final String LOCATION_DEPARTMENT_QUERY_BODY = "/repository/LocationAndDepartmentQueryBody.sql";
+    private static final String LOCATION_KEYWORD_QUERY_BODY = "/repository/LocationAndKeywordQueryBody.sql";
+    private static final String LOCATION_AND_OVERSEAS_QUERY_BODY = "/repository/LocationAndOverseasQueryBody.sql";
+    private static final String NO_OVERSEAS_PARAMETER_QUERY_BODY = "/repository/NoOverseasParameterQueryBody.sql";
 
     private SearchQueryBuilder builder;
 
@@ -44,13 +43,24 @@ public class SearchQueryBuilderTest {
 
     @Test
     public void buildCountQuery_onlyLocationSupplied() throws IOException {
-        doCountQueryTest(buildParameters(BRISTOL, null, null), LOCATION_COUNT_QUERY);
+        doCountQueryTest(buildParameters(BRISTOL, null, null, false, true, null, null), LOCATION_AND_OVERSEAS_QUERY_BODY);
     }
 
-    private SearchParameters buildParameters(String place, String keyword, String[] department) {
+    private SearchParameters buildParameters(String place, String keyword, String[] department, boolean includeRegion, boolean includeOverseasJobs, Integer minSalary, Integer maxSalary) {
         Location location = Location.builder().place(place).radius(30).build();
-        VacancySearchParameters vacancySearchParameters = VacancySearchParameters.builder().location(location).keyword(keyword).department(department).build();
+        VacancySearchParameters vacancySearchParameters = VacancySearchParameters.builder()
+                .location(location)
+                .keyword(keyword)
+                .department(department)
+                .overseasJob(includeOverseasJobs)
+                .minSalary(minSalary)
+                .maxSalary(maxSalary)
+                .build();
         Coordinates coordinates = Coordinates.builder().latitude(51.4549291).longitude(-2.6278111).build();
+
+        if (includeRegion) {
+            coordinates.setRegion("South West");
+        }
 
         return SearchParameters.builder().vacancySearchParameters(vacancySearchParameters).coordinates(coordinates).build();
     }
@@ -62,88 +72,108 @@ public class SearchQueryBuilderTest {
         try (InputStream inputStream = SearchQueryBuilderTest.class.getResourceAsStream(expectedResourceFileName)) {
 
             byte[] expectedQuery = ByteStreams.toByteArray(inputStream);
-            assertThat(actualQuery, equalTo(new String(expectedQuery)));
+            assertThat(actualQuery, equalTo("SELECT count(*) " + new String(expectedQuery)));
         }
     }
 
     @Test
     public void buildCountQuery_locationSuppliedOthersEmpty() throws IOException {
-        doCountQueryTest(buildParameters(BRISTOL, BLANK, new String[]{BLANK}), LOCATION_COUNT_QUERY);
+        doCountQueryTest(buildParameters(BRISTOL, BLANK, new String[]{BLANK}, false, true, null, null), LOCATION_AND_OVERSEAS_QUERY_BODY);
     }
 
     @Test
     public void buildSelectValuesQuery_onlyLocationSupplied() throws IOException {
-        doSelectValuesQueryTest(buildParameters(BRISTOL, null, null), LOCATION_SELECT_VALUES_QUERY);
+        doSelectValuesQueryTest(buildParameters(BRISTOL, null, null, false, true, null, null), LOCATION_AND_OVERSEAS_QUERY_BODY);
     }
 
     private void doSelectValuesQueryTest(SearchParameters parameters, String expectedResourceFileName) throws IOException {
 
-        String actualQuery = builder.buildCountQuery(parameters);
+        String actualQuery = builder.buildSelectValuesQuery(parameters);
 
         try (InputStream inputStream = SearchQueryBuilderTest.class.getResourceAsStream(expectedResourceFileName)) {
 
             byte[] expectedQuery = ByteStreams.toByteArray(inputStream);
-            assertThat(actualQuery, equalTo(new String(expectedQuery)));
+            assertThat(actualQuery, equalTo("SELECT * " + new String(expectedQuery)));
         }
     }
 
     @Test
     public void buildSelectValuesQuery_locationSuppliedOthersEmpty() throws IOException {
-        doSelectValuesQueryTest(buildParameters(BRISTOL, BLANK, new String[]{""}), LOCATION_SELECT_VALUES_QUERY);
+        doSelectValuesQueryTest(buildParameters(BRISTOL, BLANK, new String[]{BLANK}, false, true, null, null), LOCATION_AND_OVERSEAS_QUERY_BODY);
+    }
+
+    @Test
+    public void buildCountQuery_allLocationParametersRequired() throws IOException {
+        doCountQueryTest(buildParameters(BRISTOL, null, null, true, true, null, null), ALL_LOCATION_PARAMETERS_QUERY_BODY);
+    }
+
+    @Test
+    public void buildSelectValuesQuery_allLocationParametersRequired() throws IOException {
+        doSelectValuesQueryTest(buildParameters(BRISTOL, null, null, true, true, null, null), ALL_LOCATION_PARAMETERS_QUERY_BODY);
+    }
+
+    @Test
+    public void buildCountQuery_noOverseasJobsRequired() throws IOException {
+        doCountQueryTest(buildParameters(BRISTOL, null, null, true, false, null, null), NO_OVERSEAS_PARAMETER_QUERY_BODY);
+    }
+
+    @Test
+    public void buildSelectValuesQuery_noOverseasJobsRequired() throws IOException {
+        doSelectValuesQueryTest(buildParameters(BRISTOL, null, null, true, false, null, null), NO_OVERSEAS_PARAMETER_QUERY_BODY);
     }
 
     @Test
     public void buildCountQuery_locationAndKeywordSupplied() throws IOException {
-        doCountQueryTest(buildParameters(BRISTOL, KEYWORD, null), LOCATION_KEYWORD_COUNT_QUERY);
-    }
-
-    @Test
-    public void buildCountQuery_locationAndKeywordWithEmptyDepartment() throws IOException {
-        doCountQueryTest(buildParameters(BRISTOL, KEYWORD, new String[]{BLANK}), LOCATION_KEYWORD_COUNT_QUERY);
+        doCountQueryTest(buildParameters(BRISTOL, KEYWORD, null, false, true, null, null), LOCATION_KEYWORD_QUERY_BODY);
     }
 
     @Test
     public void buildSelectValuesQuery_locationAndKeywordSupplied() throws IOException {
-        doSelectValuesQueryTest(buildParameters(BRISTOL, KEYWORD, null), LOCATION_KEYWORD_SELECT_VALUES_QUERY);
+        doSelectValuesQueryTest(buildParameters(BRISTOL, KEYWORD, null, false, true, null, null), LOCATION_KEYWORD_QUERY_BODY);
     }
 
     @Test
     public void buildSelectValuesQuery_locationAndKeywordWithEmptyDepartment() throws IOException {
-        doSelectValuesQueryTest(buildParameters(BRISTOL, KEYWORD, new String[]{BLANK}), LOCATION_KEYWORD_SELECT_VALUES_QUERY);
+        doSelectValuesQueryTest(buildParameters(BRISTOL, KEYWORD, new String[]{BLANK}, false, true, null, null), LOCATION_KEYWORD_QUERY_BODY);
+    }
+
+    @Test
+    public void buildCountQuery_locationAndKeywordWithEmptyDepartment() throws IOException {
+        doCountQueryTest(buildParameters(BRISTOL, KEYWORD, new String[]{BLANK}, false, true, null, null), LOCATION_KEYWORD_QUERY_BODY);
     }
 
     @Test
     public void buildCountQuery_locationAndDepartmentSupplied() throws IOException {
-        doCountQueryTest(buildParameters(BRISTOL, null, DEPARTMENTS), LOCATION_DEPARTMENT_COUNT_QUERY);
+        doCountQueryTest(buildParameters(BRISTOL, null, DEPARTMENTS, false, true, null, null), LOCATION_DEPARTMENT_QUERY_BODY);
     }
 
     @Test
     public void buildCountQuery_locationAndDepartmentWithEmptyKeyword() throws IOException {
-        doCountQueryTest(buildParameters(BRISTOL, BLANK, DEPARTMENTS), LOCATION_DEPARTMENT_COUNT_QUERY);
+        doCountQueryTest(buildParameters(BRISTOL, BLANK, DEPARTMENTS, false, true, null, null), LOCATION_DEPARTMENT_QUERY_BODY);
     }
 
     @Test
     public void buildSelectValuesQuery_locationAndDepartmentSupplied() throws IOException {
-        doSelectValuesQueryTest(buildParameters(BRISTOL, null, DEPARTMENTS), LOCATION_DEPARTMENT_SELECT_VALUES_QUERY);
+        doSelectValuesQueryTest(buildParameters(BRISTOL, null, DEPARTMENTS, false, true, null, null), LOCATION_DEPARTMENT_QUERY_BODY);
     }
 
     @Test
     public void buildSelectValuesQuery_locationAndDepartmentWithEmptyKeyword() throws IOException {
-        doSelectValuesQueryTest(buildParameters(BRISTOL, BLANK, DEPARTMENTS), LOCATION_DEPARTMENT_SELECT_VALUES_QUERY);
+        doSelectValuesQueryTest(buildParameters(BRISTOL, BLANK, DEPARTMENTS, false, true, null, null), LOCATION_DEPARTMENT_QUERY_BODY);
     }
 
     @Test
     public void buildCountQuery_allParametersSupplied() throws IOException {
-        doCountQueryTest(buildParameters(BRISTOL, KEYWORD, DEPARTMENTS), "/repository/AllParametersSuppliedCountQuery.sql");
+        doCountQueryTest(buildParameters(BRISTOL, KEYWORD, DEPARTMENTS, true, true, 10000, 20000), ALL_PARAMETERS_QUERY_BODY);
     }
 
     @Test
     public void buildSelectValuesQuery_allParametersSupplied() throws IOException {
-        doSelectValuesQueryTest(buildParameters(BRISTOL, KEYWORD, DEPARTMENTS), ALL_PARAMETERS_SELECT_VALUES_QUERY);
+        doSelectValuesQueryTest(buildParameters(BRISTOL, KEYWORD, DEPARTMENTS, true, true, 10000, 20000), ALL_PARAMETERS_QUERY_BODY);
     }
 
     @Test
     public void buildSelectValuesQuery_allParametersSuppliedWithRogueDepartment() throws IOException {
-        doSelectValuesQueryTest(buildParameters(BRISTOL, KEYWORD, new String[]{"1", "   ", "2"}), ALL_PARAMETERS_SELECT_VALUES_QUERY);
+        doSelectValuesQueryTest(buildParameters(BRISTOL, KEYWORD, new String[]{"1", "   ", "2"}, true, true, 10000, 20000), ALL_PARAMETERS_QUERY_BODY);
     }
 }
