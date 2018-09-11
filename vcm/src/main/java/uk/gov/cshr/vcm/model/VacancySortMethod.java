9dd18a2596e2447771de918255890182ace39164
Seamus Singh <seamus.singh@amortech.com>
2018-09-11 15:13:36 +0100

    CSHRCP-1685

--- vcm/src/main/java/uk/gov/cshr/vcm/model/VacancySortMethod.java
+++ vcm/src/main/java/uk/gov/cshr/vcm/model/VacancySortMethod.java
@@ -1,6 +1,16 @@
 package uk.gov.cshr.vcm.model;
 
+/**
+ * This enum defines the possible sort methods available during search
+ *
+ * <pre>
+ *     <ul>
+ *         <li>CLOSING_DATE - will order results by descending Vacancy.closingDate</li>
+ *         <li>RECENTLY_ADDED - will order results by descending Vacancy.publicOpeningDate</li>
+ *     </ul>
+ * </pre>
+ */
 public enum VacancySortMethod {
-    DEFAULT,
-    MOST_RECENT_POSTING;
+    CLOSING_DATE,
+    RECENTLY_ADDED
 }
